ae4c9c9d46684623cba22300df1b11f317acf40a
Nishitha0730 <pitigalapknw.22@uom.lk>
2025-08-29 03:17:45 +0530

    using recursion implement mergesort

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -14,7 +14,7 @@ public class MergeSort {
         printArray(numbers);
 
 
-        mergeSort(numbers, 0, numbers.length - 1);
+        mergeSort(numbers);
 
         System.out.println("Sorted array:");
         printArray(numbers);
@@ -27,8 +27,55 @@ public class MergeSort {
         System.out.println();
     }
 
-    public static void mergeSort(int[] array, int left, int right) {
-        
+    public static void mergeSort(int[] array) {
+        if(array.length < 2) {
+            return;
+        }
+        int midIndex = array.length / 2;
+        int[] leftArray = new int[midIndex];
+        int[] rightArray = new int[array.length - midIndex];
+
+        for(int i=0;i<midIndex;i++) {
+            leftArray[i] = array[i];
+        }
+        for(int i=midIndex;i<array.length;i++) {
+            rightArray[i - midIndex] = array[i];
+        }
+
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+        merge(array, leftArray, rightArray);
+
+    }
+
+    public static void merge(int[] array, int[] leftArray, int[] rightArray) {
+        int leftSize = leftArray.length;
+        int rightSize = rightArray.length;
+        int i=0, j=0, k=0;
+
+        while(i < leftSize && j < rightSize) {
+            if(leftArray[i] <= rightArray[j]) {
+                array[k] = leftArray[i];
+                i++;
+            } else {
+                array[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i < leftSize) {
+            array[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        while(j < rightSize) {
+            array[k] = rightArray[j];
+            j++;
+            k++;
+        }
+
     }
 
 }
