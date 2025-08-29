74031c1320bf39c6798384b6e8a558978de8376e
Nishitha0730 <pitigalapknw.22@uom.lk>
2025-08-29 23:50:12 +0530

    insert Element use Recusive insert method

---
+++ Data_Structure/myBinaryTree.java
@@ -0,0 +1,43 @@
+package Data_Structure;
+
+public class myBinaryTree {
+    public static void main(String[] args){
+        System.out.println("Hello World ");
+    }
+
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data){
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    public static class BinaryTree{
+        private Node root;
+
+        public BinaryTree(){
+            root = null;
+        }
+
+        public void insert(int data){
+            root = insertRec(root, data);
+        }
+
+        public Node insertRec(Node current, int data){
+            if(current == null){
+                return new Node(data);
+            }
+            if(data < current.data){
+                current.left = insertRec(current.left, data);
+            }else{
+                current.right = insertRec(current.right, data);
+            }
+            return current;
+        }
+    }
+}
